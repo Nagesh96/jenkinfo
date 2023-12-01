c0ef35673515206ae693962d37281fa3acb51115
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-01 16:37:26 +0530

    Update Multiple _fields.py

--- Multiple _fields.py
+++ Multiple _fields.py
@@ -8,56 +8,65 @@ def update_issue_fields(username, password, issue_key, fields_dict):
         jira = JIRA(server=jira_url, basic_auth=(username, password))
         issue = jira.issue(issue_key)
         
-        issue.update(fields=fields_dict)
+        update_fields = {}
+        for field_name, new_value in fields_dict.items():
+            setattr(issue.fields, field_name, new_value)
+            update_fields[field_name] = new_value
+        
+        issue.update(fields=update_fields)
         
         print(f"Fields in issue '{issue_key}' updated successfully.")
     except Exception as e:
-        print(f"Failed to update the fields. Error:", str(e))
+        print("Failed to update the fields. Error:", str(e))
 
-if len(sys.argv) < 6 or not all(arg.startswith(('JIRA_TICKET=', 'ENVIRONMENT=', 'COMMENT=', 'USERNAME=', 'PASSWORD=')) for arg in sys.argv[1:]):
-    print("Usage: python jira_up_arg.py JIRA_TICKET=<issue_key> ENVIRONMENT=<environment> COMMENT=<comment> USERNAME=<username> PASSWORD=<password> [field_name1=value1 field_name2=value2 ...]")
+# Check if the correct number of arguments is provided
+if len(sys.argv) % 2 != 1 or len(sys.argv) < 10:
+    print("Usage: python jira_up_arg.py JIRA_TICKET <issue_key> ENVIRONMENT <environment> COMMENT <comment> USERNAME <username> PASSWORD <password> [FieldName1=new_value1 FieldName2=new_value2 ...]")
     sys.exit(1)
 
-args = {}
-for arg in sys.argv[1:]:
-    key_value = arg.split('=')
-    if len(key_value) == 2:
-        args[key_value[0]] = key_value[1]
+# Parse key-value pairs from command line arguments
+args = dict(zip(sys.argv[1::2], sys.argv[2::2]))
 
+# Retrieve the JIRA ticket, environment, and comment from the arguments
 issue_key = args.get("JIRA_TICKET")
 environment = args.get("ENVIRONMENT")
 comment = args.get("COMMENT")
 jira_username = args.get("USERNAME")
 jira_password = args.get("PASSWORD")
 
-fields_dict = {k: v for k, v in args.items() if k not in ['JIRA_TICKET', 'ENVIRONMENT', 'COMMENT', 'USERNAME', 'PASSWORD']}
-
-if not issue_key or not environment or not comment or not jira_username or not jira_password or not fields_dict:
-    print("Please provide 'JIRA_TICKET', 'ENVIRONMENT', 'COMMENT', 'USERNAME', 'PASSWORD', and at least one 'field_name'='value' pair")
+# Ensure required arguments are provided
+if not issue_key or not environment or not comment or not jira_username or not jira_password:
+    print("Please provide 'JIRA_TICKET', 'ENVIRONMENT', 'COMMENT', 'USERNAME', and 'PASSWORD'")
     sys.exit(1)
 
-jira_url = "https://jira.charter.com"
-
-status_mapping = {
-    "QA": "IN QA",
-    "UAT": "IN UAT",
-    "PROD": "READY FOR PRODUCTION"
-}
+fields_dict = {}
+for key, value in args.items():
+    if key not in ["JIRA_TICKET", "ENVIRONMENT", "COMMENT", "USERNAME", "PASSWORD"]:
+        fields_dict[key] = value
 
 try:
-    jira = JIRA(server=jira_url, basic_auth=(jira_username, jira_password))
+    # Map environment names to JIRA status
+    status_mapping = {
+        "QA": "IN QA",
+        "UAT": "IN UAT",
+        "PROD": "READY FOR PRODUCTION"
+    }
+
+    jira = JIRA(server="https://jira.charter.com", basic_auth=(jira_username, jira_password))
 
     if environment in status_mapping:
         new_status = status_mapping[environment]
-        
+
         text = f"{comment}"
 
+        # Update the JIRA issue status
         issue = jira.issue(issue_key)
         jira.transition_issue(issue, new_status)
         jira.add_comment(issue, body=text)
-        
-        update_issue_fields(jira_username, jira_password, issue_key, fields_dict)
-        
+
+        if fields_dict:
+            update_issue_fields(jira_username, jira_password, issue_key, fields_dict)
+
         print(f"JIRA issue {issue_key} status updated to {new_status}. Comment added.")
     else:
         print(f"Environment '{environment}' not found in the mapping")
