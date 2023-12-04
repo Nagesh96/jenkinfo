9c0c7fb0698ee39c2dec502cc8e9d23645bb938a
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-04 15:01:17 +0530

    Update jira_dashboard.py

--- jira_dashboard.py
+++ jira_dashboard.py
@@ -4,9 +4,17 @@ from jira import JIRA
 options = {
     'server': 'https://your-jira-instance.atlassian.net'
 }
-
-# Authenticate using your username and password
 jira = JIRA(options, basic_auth=('your_username', 'your_password'))
+
+# Define the dashboard details
+dashboard_name = 'My Python Dashboard'
+dashboard_description = 'Dashboard created using Python'
+
+# Create the dashboard
+dashboard = jira.create_dashboard(dashboard_name, description=dashboard_description)
+
+# Print the dashboard URL
+print(f"Dashboard created! URL: {dashboard.permalink()}")
 
 # Manipulate version field value in the JQL query
 version_name = '1.0'  # Example version name
